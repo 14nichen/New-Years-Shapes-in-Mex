52a17ecc790678a6ce30126d775b9e23281cd0f6
14nichen <nchen@CART115-LAP09.local>
2014-02-03 13:02:27 -1000

    Mex + Chi = Chex
    Mexi + Chin = Chexy

---
+++ f.rb
@@ -0,0 +1,66 @@
+size 300,300
+#NO NEED size 550,550
+#no_stroke
+#rect 10,10,60,100
+#ellipse 100,260,45,75
+
+#stroke 255
+#fill 0
+#rect 120,100,100,40
+
+stroke 255
+fill 0
+#rect 25,25,500,500
+rect 25,25,250,250
+
+stroke 255
+fill 255
+
+#NO NEED BIG DIAGONAL
+#rect 25,275,250,250
+#rect 275,25,250,250
+
+#diagonals
+fill 127.5
+rect 150,25,125,125
+rect 25,150,125,125
+
+#top left
+fill 255
+stroke 255
+rect 87.5,25,62.5,62.5
+rect 25,87.5,62.5,62.5
+rect 25,25,62.5,62.5
+
+#top top left only part one
+#rect 56.25,25,31.25,31.25
+#rect 25,56.25,31.25,31.25
+
+#all only part two
+#bottom right
+fill 0
+stroke 255
+rect 212.5,150,62.5,62.5
+rect 150,212.5,62.5,62.5
+
+fill 191.25
+rect 87.5,87.5,62.5,62.5
+
+fill 67.5
+rect 150,150,62.5,62.5
+
+stroke 255
+#bottom left
+fill 191.25
+rect 25,150,62.5,62.5
+fill 63.75
+rect 87.5,212.5,62.5,62.5
+
+#top right
+fill 191.25
+rect 150,25,62.5,62.5
+fill 63.75
+rect 212.5,87.5,62.5,62.5
+
+
+
